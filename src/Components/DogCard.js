e4ea3ed796c651b3a922ae8f371287719b299a9e
jrwatson94 <j.watsonreid@gmail.com>
2020-11-17 10:40:41 -0500

    Done

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -1,17 +1,67 @@
 import React from "react";
 
-function DogCard() {
-  return (
-    <div className="card">
-      <span className="content">
-        <h2 >{/*Dog name goes here*/}</h2>
-        <img alt="" src={""} />
-      </span>
-      <span className="bark">
-        <button>Bark</button>
-      </span>
-    </div>
-  );
+export default class DogCard extends React.Component{
+  constructor(props){
+    super(props)
+    this.state ={
+      active: ""
+    }
+  }
+  displayBark = () => {
+    if (this.state.active === ""){
+      console.log("active")
+      this.setState({active:"Bark!"})
+      
+    }else{
+      console.log("inactive")
+      this.setState({active:""})
+    }
+  }
+  
+  render(){
+    return (
+      <div className="card">
+        <span className="content">
+          <h2 >{this.props.name}</h2>
+          <img alt="dog" src={this.props.img} />
+        </span>
+        <span className="bark">
+          <button onClick={ this.displayBark }>Bark</button>
+        </span>
+    <h2 className="bark-display">{this.state.active}</h2>
+      </div>
+    );
+  }
 }
 
-export default DogCard;
+// function DogCard(props) {
+//   let h2 = <h2>Bark!</h2>
+//   let innerText = "Bark!"
+
+//   let displayBark = () => {
+//     if (props.active == "inactive"){
+//       console.log("active")
+//       innerText = "Bark!"
+      
+//     }else{
+//       console.log("inactive")
+//       innerText = ""
+//       clicked = !clicked
+//     }
+//   }
+  
+//   return (
+//     <div className="card">
+//       <span className="content">
+//         <h2 >{props.name}</h2>
+//         <img alt="dog" src={props.img} />
+//       </span>
+//       <span className="bark">
+//         <button onClick={ displayBark }>Bark</button>
+//       </span>
+//       <h2 className="bark-display"></h2>
+//     </div>
+//   );
+// }
+
+// export default DogCard;
--- src/Containers/DogsList.js
+++ src/Containers/DogsList.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import { apiResponse } from "../api"
+import DogCard from "../Components/DogCard"
 
 class DogsList extends Component {
+
+  renderDogCard = () => {
+    return apiResponse.map(dog => <DogCard name= {dog.name} img = {dog.img}/>)
+  }
+
+
   render() {
-    return <div className="list">{/*list of dogs go here*/}</div>;
+  return <div className="list">{this.renderDogCard()}</div>;
   }
 }
 
